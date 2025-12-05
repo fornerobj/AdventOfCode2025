dbe53d7a5d554f40dee0214aca4b39e2a426e940
Brandon Fornero <fornerobj@gmail.com>
2025-12-05 11:37:46 -0600

    Refactored

--- day5/d5.py
+++ day5/d5.py
@@ -1,10 +1,11 @@
 def check_in_range(range, ingredient):
-    l,h = r.split("-")
-    if int(ingredient) >= int(l) and int(ingredient) <= int(h):
+    l,h = range
+    if int(ingredient) >= l and int(ingredient) <= h:
         return True
     return False
 
 def reduce_ranges(fresh):
+    fresh = sorted(fresh)
     i = 0
     while i < len(fresh)-1:
         l1, h1 = fresh[i]
@@ -25,22 +26,20 @@ with open("input", "r") as f:
     db = f.read().strip()
     ranges, ingredients = db.split("\n\n")
 
-    pt1 = 0
-    for ing in ingredients.splitlines():
-        for r in ranges.splitlines():
-            if check_in_range(r, ing):
-                pt1 += 1
-                break
-    print("Part 1:", pt1)
-
     fresh = []
     for r in ranges.splitlines():
         l, h = r.split("-")
         l, h = int(l), int(h)
         fresh.append((l,h))
-    fresh = sorted(fresh)
-
     fresh = reduce_ranges(fresh)
+
+    pt1 = 0
+    for ing in ingredients.splitlines():
+        for f in fresh:
+            if check_in_range(f, ing):
+                pt1 += 1
+                break
+    print("Part 1:", pt1)
 
     pt2 = 0
     for r in fresh:
